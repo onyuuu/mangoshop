5aa24ee4074778164b7667f094d8621e0a0379ae
onyuuu <121140435+onyuuu@users.noreply.github.com>
2023-02-10 13:14:08 +0900

    재수정

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,1 @@
-export const API_URL= process.env.NODE_ENV === 'production' ? 'https://port-0-mango-server-luj2cldxxhogb.sel3.cloudtype.app/':'http://localhost:8080';
+export const API_URL= process.env.NODE_ENV === 'production' ? 'https://port-0-mango-server-luj2cldxxhogb.sel3.cloudtype.app':'http://localhost:8080';
